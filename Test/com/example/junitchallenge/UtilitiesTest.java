e7934494c668532f2b4586ac09b889181308145a
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-18 20:51:44 +0100

    Finished all tests

--- Test/com/example/junitchallenge/UtilitiesTest.java
+++ Test/com/example/junitchallenge/UtilitiesTest.java
@@ -4,14 +4,23 @@ import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
+    private Utilities util;
+
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
+    }
+
     @org.junit.Test
     public void everyNtChar() {
-        fail("This test has not been implemented");
+        char[] output = util.everyNtChar( new char[] {'h', 'e', 'l', 'l', 'o'}, 2 );
+        assertArrayEquals( new char[] {'e', 'l'}, output );
+        char[] output2 = util.everyNtChar( new char[] {'h', 'e', 'l', 'l', 'o'}, 8 );
+        assertArrayEquals( new char[] {'h', 'e', 'l', 'l', 'o', }, output2);
     }
 
     @org.junit.Test
     public void removePairs() {
-        Utilities util = new Utilities();
         assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
         assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
         assertNull("Did not get null returned when argument passed was null", util.removePairs(null));
@@ -21,11 +30,17 @@ public class UtilitiesTest {
 
     @org.junit.Test
     public void converter() {
-        fail("This test has not been implemented");
+        assertEquals( 300, util.converter( 10, 5));
+    }
+
+    @org.junit.Test( expected = ArithmeticException.class )
+    public void converter_AritmeticException() throws Exception {
+        util.converter(10, 0);
     }
 
     @org.junit.Test
     public void nullIfOddLenght() {
-        fail("This test has not been implemented");
+        assertNull( util.nullIfOddLenght("odd") );
+        assertNotNull( util.nullIfOddLenght("even") );
     }
 }
